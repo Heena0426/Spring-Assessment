f023d036aaa3dc50ca46dc95a3710f8bc849f7d4
Heena <dongarkarheena978@gmail.com>
2024-08-20 19:45:56 +0530

    Application is running on port 8080

---
+++ assignment_one/src/test/java/com/assignment1/assignment_one/AssignmentOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment1.assignment_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
